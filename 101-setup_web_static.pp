5b88cd9150f3f0109d6571bd9e373392a14d0ae1
“aniekanmark55” <aniekanmark55@gmail.com>
2023-05-05 06:23:01 -0700

    v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,7 +12,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 https://www.linkedin.com/in/aniekan-mark-5726b6196;
     }
     error_page 404 /404.html;
     location /404 {
